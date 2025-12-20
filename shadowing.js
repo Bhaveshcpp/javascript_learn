593a04a0f4e98a90364bdf506a3946e15c74eb1d
Bhaveshcpp <bhaveshsh2004@gmail.com>
2025-12-20 13:59:00 +0000

    shadowing in JS

---
+++ shadowing.js
@@ -0,0 +1,17 @@
+{
+let a=10
+const b=20
+var c=30;
+}
+const a=100;
+var c=40;
+console.log(c);
+console.log(a);
+// console.log(b);
+
+// //ILLEGAL SHADIWING//
+// let x=20;
+// {
+//     var x=10;
+// }
+// console.log(x);

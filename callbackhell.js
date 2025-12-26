cde28133a1a1ed21924ba9430519ecf427193e9e
Bhaveshcpp <bhaveshsh2004@gmail.com>
2025-12-26 11:16:15 +0000

    Callback plus and minus

---
+++ callbackhell.js
@@ -0,0 +1,6 @@
+//The async operation in javascript is performed using callbacks
+//Callback are very important part of JS 
+
+//Issues with callbacks are that 
+//a.Callback hell- when there are callback inside callback and goes and code becomes unmaintable//
+//b.Inversion of control-When we loose control of the function by passing it to another function and we dont know will that ever execute or not//
